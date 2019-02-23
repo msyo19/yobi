07210130769752802dbef5e1ca8764c7379cac54
Jorge Carranza <jorgemsyo17@gmail.com>
2019-02-23 17:17:18 -0600

    esqueleto login

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/test', function(req, res, next, err) {
-  if (err) {
-    res.status(400).send(err)
-  } else {
-    res.status(200).json({
-      status: true, 
-      message: "Success"
-    });
-  }
+router.post('/test', function(req, res, next) {
+  res.status = 400;
+});
+
+router.get('/test', function(req, res, next) {
+  res.send('hola get')
+});
+
+
+router.get('/*', function(req, res, next) {
+  console.log('jajaja');
 });
 
 module.exports = router;
