6c390dd25233b568a79fe3e56ebe8e0fbdc16c1f
Allie <allie_michell@hotmail.com>
2019-02-23 14:59:11 -0600

    User Schema

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/test', function(req, res, next, err) {
+  if (err) {
+    res.status(400).send(err)
+  } else {
+    res.status(200).json({
+      status: true, 
+      message: "Success"
+    });
+  }
 });
 
 module.exports = router;
---
+++ src/models/users.js
@@ -0,0 +1,43 @@
+var mongoose =  require('mongoose');
+
+var userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String
+        }, 
+        address: {
+            type: String
+        }, 
+        email:{
+            type: String
+        }, 
+        password: {
+            type: String
+        },
+        phone: {
+            type: Number
+        }, 
+        area : {
+            type: String
+        }, 
+        status: {
+            type: Boolean
+        }, 
+        comments: {
+            type: String 
+        }, 
+        rate: {
+            type: String
+        }, 
+        services: {
+            type: String
+        }
+
+    },
+    {
+        collection: 'Users'
+    }
+); 
+
+var Users = mongoose.model('Users', userSchema); 
+module.export = Users;
