55023f7592ff9bed19fdc0f5d9fe6bbc939b24de
Allie <allie_michell@hotmail.com>
2019-02-24 02:46:21 -0600

    rutas

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.post('/test', function(req, res, next) {
   res.status = 400;
 });
 
-router.get('/test', function(req, res, next) {
-  res.send('hola get')
+router.get('/index', function(req, res, next) {
+  res.sendFile(path.resolve('public', 'index.html'));
 });
 
+router.get('/inicio', function (req, res, next) {
+  res.sendFile(path.resolve('public', 'inicio.html'));
+});
 
+router.get('/registerTrabajo', function (req, res, next) {
+  res.sendFile(path.resolve('public', 'registerTrabajo.html'));
+});
+
+router.get('/servicios', function (req, res, next) {
+  res.sendFile(path.resolve('public', 'servicios.html'));
+});
 module.exports = router;
